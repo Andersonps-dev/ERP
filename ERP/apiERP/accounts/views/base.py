3d655d4c3cbb1e443c74642306c278d4324a7cb9
Andersonps-dev <aaandersonpereira45265@gmail.com>
2025-04-29 21:19:58 -0300

    Refactor base.py to improve user enterprise retrieval logic and enhance permission handling

--- ERP/apiERP/accounts/views/base.py
+++ ERP/apiERP/accounts/views/base.py
@@ -1,6 +1,9 @@
 from rest_framework.views import APIView
+from rest_framework.exceptions import APIException
 
-from companies.models import Enterprise
+from accounts.models import User_Groups, Group_Permissions
+
+from companies.models import Enterprise, Employee
 
 class Base(APIView):
     def get_enterprise_user(self, user_id) -> None:
@@ -12,3 +15,23 @@ class Base(APIView):
         enterprise['is_owner'] = Enterprise.objects.filter(user=user_id).exists()
 
         if enterprise['is_owner']: return enterprise
+
+        employee = Employee.objects.filter(user=user_id).first()
+
+        if not employee: raise APIException("Este usuário não pertence a nenhuma empresa.")
+
+        groups = User_Groups.objects.filter(user=user_id).all()
+
+        for g in groups:
+            group = g.group
+
+            permission = Group_Permissions.objects.filter(group_id=group.id).all()
+            
+            for p in permission:
+                enterprise['permissions'].append({
+                    "id": p.permission.id,
+                    "label": p.permission.name,
+                    "codename": p.permission.codename,
+                })
+
+        return enterprise
